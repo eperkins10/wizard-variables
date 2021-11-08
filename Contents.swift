a812c34c45d8ee913d2c50366320af783705325b
Ethan Perkins <ethanperkinspro@gmail.com>
2021-11-08 16:11:39 -0700

    first commit

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Part One
+let firstName = "Harry"
+print(firstName)
+let lastName = "Potter"
+let birthday = "July 31, 1980"
+let nationality = "English"
+let bloodStatus = "half-blood"
+let gender = "Male"
+let height = 164
+let weight = 140
+
+//Part Two
+let appearedInHarryMovie = true
+let appearedInFantasticBeasts = false
+let dumbledoresArmy = true
+let deathEater = false
+
+//Part Three
+let importance = "crucial"
+let rating = 10
+let favoriteCharReason = "because he's who it's all about.  The movie is literally named after him"
